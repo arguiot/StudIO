93fce5efa65f50ec9e74cb5a6a3f2f7ea682c972
Arthur Guiot <arguiot@gmail.com>
2018-12-02 20:03:04 +0800

    All black design

--- StudIO/AppDelegate.swift
+++ StudIO/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UIApplication.shared.statusBarStyle = .lightContent
         return true
     }
     func applicationWillResignActive(_ application: UIApplication) {
