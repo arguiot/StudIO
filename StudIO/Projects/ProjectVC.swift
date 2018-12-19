12a009fe5786d85ff5041c5ec9d3c7992c145f4d
Arthur Guiot <arguiot@gmail.com>
2018-12-19 19:43:26 +0800

    Added the feature to use custom credentials in clone

--- StudIO/Projects/ProjectVC.swift
+++ StudIO/Projects/ProjectVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import BLTNBoard
+import SwiftGit2
 
 class ProjectVC: UICollectionViewController {
     
@@ -108,12 +109,16 @@ class ProjectVC: UICollectionViewController {
         return BLTNItemManager(rootItem: page)
     }
     
+    var creds: Credentials = .default
+    
     func clone(_ page: TextFieldBulletinPage) -> BLTNPageItem {
         page.descriptionText = "Enter the Git repository URL you would like to clone."
         page.actionButtonTitle = "Done"
+        page.alternativeButtonTitle = "Clone using custom credentials"
         
         var can = false
         var url: URL?
+        
         page.textInputHandler = { item, text in
             can = true
             url = URL(string: text!)
@@ -145,6 +150,49 @@ class ProjectVC: UICollectionViewController {
                 item.manager?.displayNextItem()
             }
         }
+        page.alternativeHandler = { (item: BLTNActionItem) in
+            let alertWindow = UIWindow(frame: UIScreen.main.bounds)
+            alertWindow.rootViewController = UIViewController()
+            alertWindow.windowLevel = UIWindow.Level.alert + 1
+            alertWindow.makeKeyAndVisible()
+            
+            //1. Create the alert controller.
+            let alert = UIAlertController(title: "Username", message: "Please enter your git username", preferredStyle: .alert)
+            
+            //2. Add the text field. You can configure it however you need.
+            alert.addTextField { (textField) in
+                textField.placeholder = "username"
+            }
+            
+            // 3. Grab the value from the text field, and print it when the user clicks OK.
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+                let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
+                let username = textField?.text
+                alertWindow.rootViewController?.dismiss(animated: true, completion: nil)
+                //1. Create the alert controller.
+                let alert = UIAlertController(title: "Password", message: "Please enter your git password", preferredStyle: .alert)
+                
+                //2. Add the text field. You can configure it however you need.
+                alert.addTextField { (textField) in
+                    textField.placeholder = "password"
+                    textField.isSecureTextEntry = true
+                }
+                
+                // 3. Grab the value from the text field, and print it when the user clicks OK.
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+                    let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
+                    let password = textField?.text
+                    self.creds = .plaintext(username: username ?? "", password: password ?? "")
+                    alertWindow.rootViewController?.dismiss(animated: true, completion: nil)
+                }))
+                
+                // 4. Present the alert.
+                alertWindow.rootViewController?.present(alert, animated: true, completion: nil)
+            }))
+            
+            alertWindow.rootViewController?.present(alert, animated: true, completion: nil)
+        }
+        
         return page
     }
     
