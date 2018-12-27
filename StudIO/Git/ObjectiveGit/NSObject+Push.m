9e3ba519043197eb53e282a47c0aec773119ce59
Arthur Guiot <arguiot@gmail.com>
2018-12-27 10:33:07 +0100

    Added push method in ObjC

--- StudIO/Git/ObjectiveGit/NSObject+Push.m
+++ StudIO/Git/ObjectiveGit/NSObject+Push.m
@@ -11,7 +11,21 @@
 
 @implementation NSObject (Push)
 - (BOOL)push:(NSURL*)url {
-    GTRepository* repo = [GTRepository repositoryWithURL:url error:(NSError * _Nullable __autoreleasing * _Nullable)];
+    NSDictionary *userInfo = @{
+                               NSLocalizedDescriptionKey: NSLocalizedString(@"Couldn't find repository", nil),
+                               NSLocalizedFailureReasonErrorKey: NSLocalizedString(@"The operation timed out.", nil),
+                               NSLocalizedRecoverySuggestionErrorKey: NSLocalizedString(@"Try to force close the app and do it again.", nil)
+                               };
+    NSError  __autoreleasing * _Nullable error = [NSError errorWithDomain:GTFilterErrorDomain
+                                         code:-57
+                                     userInfo:userInfo];
     
+    GTRepository* repo = [GTRepository repositoryWithURL:url error:&error];
+    git_push_options *opts = (__bridge git_push_options *)(@{});
+    git_strarray arr;
+    git_remote_list(&arr, (__bridge git_repository *)(repo));
+    GTRemote* remote = [GTRemote remoteWithName:@"master" inRepository:repo error:&error];
+    git_remote_push(remote.git_remote, NULL, opts);
+    return true;
 }
 @end
