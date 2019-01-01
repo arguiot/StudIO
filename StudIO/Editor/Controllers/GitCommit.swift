691a498088551783d392bed7553f24ebe9c17d86
Arthur Guiot <arguiot@gmail.com>
2019-01-01 13:33:44 +0100

    Added GitCommit view, on the right of editor view

---
+++ StudIO/Editor/Controllers/GitCommit.swift
@@ -0,0 +1,57 @@
+//
+//  GitCommit.swift
+//  StudIO
+//
+//  Created by Arthur Guiot on 1/1/19.
+//  Copyright © 2019 Arthur Guiot. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class GitCommit: UIView {
+    @IBOutlet var contentView: UIView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var commitStrip: UITextView!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("GitCommit", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        initialisation()
+        
+    }
+    func initialisation() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.frame.origin.y == 0 {
+                self.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.frame.origin.y != 0 {
+            self.frame.origin.y = 0
+        }
+    }
+    
+    @IBAction func commit(_ sender: Any) {
+    }
+}
