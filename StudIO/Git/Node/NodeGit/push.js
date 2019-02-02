ce456d58a8e7bc9ee0181735857d361e18c23096
Arthur Guiot <arguiot@gmail.com>
2019-02-02 11:16:20 +0800

    Need to fix git and fs

--- StudIO/Git/Node/NodeGit/push.js
+++ StudIO/Git/Node/NodeGit/push.js
@@ -2,17 +2,24 @@ const git = require('isomorphic-git');
 const fs = require('fs');
 git.plugins.set('fs', fs)
 const http = require('http');
-console.log(`Pushing repo at: ${rPath}`)
-async function push() {
+// console.log(`Pushing repo at: ${process.argv[2]}`)
+(async function push() {
 	const rPath = process.argv[2]
 	const email = process.argv[3]
 	const passwd = process.argv[4]
-	const pushResponse = await git.push({
-		dir: rPath,
-		remote: 'origin',
-		ref: 'master',
-		username: email,
-		password: passwd
-	})
-	console.log(`Pushed repo at ${rPath} to origin.\nAnswer: ${pushResponse}`)
-}()
+	console.log(rPath, email, passwd)
+	try {
+ const commits = await git.log({ dir: rPath, depth: 5 })
+        console.log(commits)
+		const pushResponse = await git.push({
+			dir: rPath,
+			remote: 'origin',
+			ref: 'master',
+			authUsername: email,
+			authPassword: passwd
+		})
+		console.log(`Pushed repo at ${rPath} to origin.\nAnswer: ${pushResponse}`)
+	} catch(e) {
+		console.log(e)
+	}
+})()
