8e0c0336cd88300ddd9d756bcd487d1853013e2a
Arthur Guiot <arguiot@gmail.com>
2019-03-22 18:49:09 +0800

    Improved GitHub gist snippet

--- StudIO/Extensions/Gist.swift
+++ StudIO/Extensions/Gist.swift
@@ -13,4 +13,63 @@ class GitHubGist {
     static func isOk(_ str: String) -> Bool {
         return str.match(patternString: regex)
     }
+    
+    func download(str: String, completion: @escaping (String) -> Void) {
+        let splits = str.split(separator: "/")
+        
+        var id = ""
+        
+        splits.forEach { (substr) in
+            let s = String(substr)
+            if s != "" {
+                id = s
+            }
+        }
+        
+        let query = "https://api.github.com/gists/\(id)"
+        
+        dJSON(str: query) { (dic) in
+            let files = dic["files"] as! [String: Any]
+            
+            let keys = Array(files.keys)
+            
+            let first = String(keys[0])
+            
+            let dFiles = files[first] as! [String: Any]
+            let url = dFiles["raw_url"] as! String
+            
+            self.down(load: url, completion: completion)
+            
+        }
+    }
+    
+    func dJSON(str: String, completion: @escaping ([String: Any]) -> Void) {
+        URLSession.shared.dataTask(with: URL(string: str)!) { (data, response, error) -> Void in
+            // Check if data was received successfully
+            if error == nil && data != nil {
+                do {
+                    // Convert to dictionary where keys are of type String, and values are of any type
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: Any]
+                    
+                    completion(json)
+                } catch {
+                    // Something went wrong
+                }
+            }
+        }.resume()
+    }
+    
+    func down(load: String, completion: @escaping (String) -> Void) {
+        URLSession.shared.dataTask(with: URL(string: load)!) { (data, response, error) -> Void in
+            // Check if data was received successfully
+            if error == nil && data != nil {
+                do {
+                    let str = String(data: data!, encoding: .utf8)
+                    completion(str ?? "")
+                } catch {
+                    // Something went wrong
+                }
+            }
+        }.resume()
+    }
 }
