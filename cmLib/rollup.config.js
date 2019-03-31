017830c7a4c786b586a0c7671ec718fb6f9e1485
Arthur Guiot <arguiot@gmail.com>
2019-03-31 20:17:25 +0800

    Improved build

--- cmLib/rollup.config.js
+++ cmLib/rollup.config.js
@@ -1,23 +1,29 @@
 import typescript from "rollup-plugin-typescript2"
 import commonjs from "rollup-plugin-commonjs"
-import nodeResolve from "rollup-plugin-node-resolve"
+import resolve from "rollup-plugin-node-resolve"
 
 export default {
-  input: "./lib.ts",
-  output: {
-    format: "umd",
-    file: "../StudIO/Editor/CodeMirror/EditorView/libCM.js",
-    sourcemap: true
-  },
-  plugins: [
-    nodeResolve(),
-    typescript({
-      check: true,
-      tsconfigOverride: {
-        compilerOptions: {lib: ["es5", "es6", "dom"], sourceMap: true, target: "es5", strict: false},
-        include: null
-      }
-    }),
-    commonjs()
-  ]
+	input: "./lib.ts",
+	output: {
+		format: "umd",
+		file: "../StudIO/Editor/CodeMirror/EditorView/libCM.js",
+		sourcemap: true
+	},
+	plugins: [
+		resolve(),
+		typescript({
+			check: true,
+			tsconfigOverride: {
+				compilerOptions: {
+					lib: ["es5", "es6", "dom"],
+					sourceMap: true,
+					target: "es5",
+					strict: false
+				},
+				include: null
+			},
+
+		}),
+		commonjs()
+	]
 }
