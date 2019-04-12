d75fd6a851d651c9ff08410885b8c1f6bec710a9
Arthur Guiot <arguiot@gmail.com>
2019-04-12 18:44:01 +0800

    Converted Lua

--- StudIO/Editor/CodeMirror/EditorView/mode/lua/lua.js
+++ StudIO/Editor/CodeMirror/EditorView/mode/lua/lua.js
@@ -5,17 +5,9 @@
 // CodeMirror 1 mode.
 // highlights keywords, strings, comments (no leveling supported! ("[==[")), tokens, basic indenting
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
 "use strict";
 
-CodeMirror.defineMode("lua", function(config, parserConfig) {
+function ExportedMode(config, parserConfig) {
   var indentUnit = config.indentUnit;
 
   function prefixRE(words) {
@@ -152,8 +144,4 @@ CodeMirror.defineMode("lua", function(config, parserConfig) {
     blockCommentStart: "--[[",
     blockCommentEnd: "]]"
   };
-});
-
-CodeMirror.defineMIME("text/x-lua", "lua");
-
-});
+}
