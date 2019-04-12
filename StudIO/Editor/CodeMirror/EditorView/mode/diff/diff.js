5a313699df59b88431cda762402d5a64ec36da13
Arthur Guiot <arguiot@gmail.com>
2019-04-12 19:50:56 +0800

    Converted Diff

--- StudIO/Editor/CodeMirror/EditorView/mode/diff/diff.js
+++ StudIO/Editor/CodeMirror/EditorView/mode/diff/diff.js
@@ -1,17 +1,9 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
 "use strict";
 
-CodeMirror.defineMode("diff", function() {
+function ExportedMode() {
 
   var TOKEN_NAMES = {
     '+': 'positive',
@@ -40,8 +32,4 @@ CodeMirror.defineMode("diff", function() {
       return token_name;
     }
   };
-});
-
-CodeMirror.defineMIME("text/x-diff", "diff");
-
-});
+}
