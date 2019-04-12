e520757eaca4294020b74499d245117af735118a
Arthur Guiot <arguiot@gmail.com>
2019-04-12 19:38:48 +0800

    Converted CoffeeScript

--- StudIO/Editor/CodeMirror/EditorView/mode/coffeescript/coffeescript.js
+++ StudIO/Editor/CodeMirror/EditorView/mode/coffeescript/coffeescript.js
@@ -5,17 +5,9 @@
  * Link to the project's GitHub page:
  * https://github.com/pickhardt/coffeescript-codemirror-mode
  */
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
 "use strict";
 
-CodeMirror.defineMode("coffeescript", function(conf, parserConf) {
+function ExportedMode(conf, parserConf) {
   var ERRORCLASS = "error";
 
   function wordRegexp(words) {
@@ -347,13 +339,4 @@ CodeMirror.defineMode("coffeescript", function(conf, parserConf) {
     fold: "indent"
   };
   return external;
-});
-
-// IANA registered media type
-// https://www.iana.org/assignments/media-types/
-CodeMirror.defineMIME("application/vnd.coffeescript", "coffeescript");
-
-CodeMirror.defineMIME("text/x-coffeescript", "coffeescript");
-CodeMirror.defineMIME("text/coffeescript", "coffeescript");
-
-});
+}
