da110fabf160000e832067464f2eb267ee91efcc
Arthur Guiot <arguiot@gmail.com>
2019-04-12 19:37:27 +0800

    Converted CMake

--- StudIO/Editor/CodeMirror/EditorView/mode/cmake/cmake.js
+++ StudIO/Editor/CodeMirror/EditorView/mode/cmake/cmake.js
@@ -1,17 +1,9 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object")
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd)
-    define(["../../lib/codemirror"], mod);
-  else
-    mod(CodeMirror);
-})(function(CodeMirror) {
 "use strict";
 
-CodeMirror.defineMode("cmake", function () {
+function ExportedMode() {
   var variable_regex = /({)?[a-zA-Z0-9_]+(})?/;
 
   function tokenString(stream, state) {
@@ -90,8 +82,4 @@ CodeMirror.defineMode("cmake", function () {
       return tokenize(stream, state);
     }
   };
-});
-
-CodeMirror.defineMIME("text/x-cmake", "cmake");
-
-});
+}
