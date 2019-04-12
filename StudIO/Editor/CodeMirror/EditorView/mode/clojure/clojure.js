f62d54987d58814191f564b7f95cdf8dc981a3ef
Arthur Guiot <arguiot@gmail.com>
2019-04-12 19:36:52 +0800

    Converted clojure

--- StudIO/Editor/CodeMirror/EditorView/mode/clojure/clojure.js
+++ StudIO/Editor/CodeMirror/EditorView/mode/clojure/clojure.js
@@ -1,17 +1,9 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
-(function(mod) {
-  if (typeof exports === "object" && typeof module === "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define === "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
 "use strict";
 
-CodeMirror.defineMode("clojure", function (options) {
+function ExportedMode(options) {
   var atoms = ["false", "nil", "true"];
   var specialForms = [".", "catch", "def", "do", "if", "monitor-enter",
       "monitor-exit", "new", "quote", "recur", "set!", "throw", "try", "var"];
@@ -149,8 +141,8 @@ CodeMirror.defineMode("clojure", function (options) {
       "with-loading-context", "with-local-vars", "with-meta", "with-open",
       "with-out-str", "with-precision", "with-redefs", "with-redefs-fn"];
 
-  CodeMirror.registerHelper("hintWords", "clojure",
-    [].concat(atoms, specialForms, coreSymbols));
+  // CodeMirror.registerHelper("hintWords", "clojure",
+  //   [].concat(atoms, specialForms, coreSymbols));
 
   var atom = createLookupMap(atoms);
   var specialForm = createLookupMap(specialForms);
@@ -283,10 +275,8 @@ CodeMirror.defineMode("clojure", function (options) {
     closeBrackets: {pairs: "()[]{}\"\""},
     lineComment: ";;"
   };
-});
-
-CodeMirror.defineMIME("text/x-clojure", "clojure");
-CodeMirror.defineMIME("text/x-clojurescript", "clojure");
-CodeMirror.defineMIME("application/edn", "clojure");
-
-});
+}
+//
+// CodeMirror.defineMIME("text/x-clojure", "clojure");
+// CodeMirror.defineMIME("text/x-clojurescript", "clojure");
+// CodeMirror.defineMIME("application/edn", "clojure");
