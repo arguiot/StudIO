abcbe81c92a18d5114445c220b42c7732202ea65
Arthur Guiot <arguiot@gmail.com>
2019-04-12 18:43:17 +0800

    Converted markdown

--- StudIO/Editor/CodeMirror/EditorView/mode/markdown/markdown.js
+++ StudIO/Editor/CodeMirror/EditorView/mode/markdown/markdown.js
@@ -1,17 +1,9 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"), require("../xml/xml"), require("../meta"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror", "../xml/xml", "../meta"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
 "use strict";
 
-CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
+function ExportedMode(cmCfg, modeCfg) {
 
   var htmlMode = CodeMirror.getMode(cmCfg, "text/html");
   var htmlModeMissing = htmlMode.name == "null"
@@ -875,10 +867,8 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
     fold: "markdown"
   };
   return mode;
-}, "xml");
+}
 
-CodeMirror.defineMIME("text/markdown", "markdown");
-
-CodeMirror.defineMIME("text/x-markdown", "markdown");
-
-});
+// CodeMirror.defineMIME("text/markdown", "markdown");
+//
+// CodeMirror.defineMIME("text/x-markdown", "markdown");
