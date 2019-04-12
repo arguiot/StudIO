0ed1fb7a21a8e615ff48e5683d67d8f2c970bbc1
Arthur Guiot <arguiot@gmail.com>
2019-04-12 18:47:47 +0800

    Converted asterisk

--- StudIO/Editor/CodeMirror/EditorView/mode/asterisk/asterisk.js
+++ StudIO/Editor/CodeMirror/EditorView/mode/asterisk/asterisk.js
@@ -17,17 +17,10 @@
  * =====================================================================================
  */
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
+
 "use strict";
 
-CodeMirror.defineMode("asterisk", function() {
+function ExportedMode() {
   var atoms    = ["exten", "same", "include","ignorepat","switch"],
       dpcmd    = ["#include","#exec"],
       apps     = [
@@ -189,8 +182,4 @@ CodeMirror.defineMode("asterisk", function() {
       return null;
     }
   };
-});
-
-CodeMirror.defineMIME("text/x-asterisk", "asterisk");
-
-});
+}
