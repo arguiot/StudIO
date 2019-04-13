89237d12de0b47122c753efe2ad3cc6942345075
Arthur Guiot <arguiot@gmail.com>
2019-04-13 16:15:28 +0800

    Converted Gas

--- StudIO/Editor/CodeMirror/EditorView/mode/gas/gas.js
+++ StudIO/Editor/CodeMirror/EditorView/mode/gas/gas.js
@@ -1,17 +1,10 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
+
 "use strict";
 
-CodeMirror.defineMode("gas", function(_config, parserConfig) {
+function ExportedMode(_config, parserConfig) {
   'use strict';
 
   // If an architecture is specified, its initialization function may
@@ -340,6 +333,4 @@ CodeMirror.defineMode("gas", function(_config, parserConfig) {
     blockCommentStart: "/*",
     blockCommentEnd: "*/"
   };
-});
-
-});
+}
