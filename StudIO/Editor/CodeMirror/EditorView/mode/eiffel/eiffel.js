03166e81e3fbe3b15eafd22a3a5c6733817676ae
Arthur Guiot <arguiot@gmail.com>
2019-04-13 16:11:10 +0800

    Converted Eiffel

--- StudIO/Editor/CodeMirror/EditorView/mode/eiffel/eiffel.js
+++ StudIO/Editor/CodeMirror/EditorView/mode/eiffel/eiffel.js
@@ -1,17 +1,9 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
 "use strict";
 
-CodeMirror.defineMode("eiffel", function() {
+function ExportedMode() {
   function wordObj(words) {
     var o = {};
     for (var i = 0, e = words.length; i < e; ++i) o[words[i]] = true;
@@ -153,8 +145,4 @@ CodeMirror.defineMode("eiffel", function() {
     },
     lineComment: "--"
   };
-});
-
-CodeMirror.defineMIME("text/x-eiffel", "eiffel");
-
-});
+}
