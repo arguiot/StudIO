1ebe766b9c49713443a9d9dbb64c357d669d0bd9
Arthur Guiot <arguiot@gmail.com>
2019-04-13 16:10:42 +0800

    Converted ECL

--- StudIO/Editor/CodeMirror/EditorView/mode/ecl/ecl.js
+++ StudIO/Editor/CodeMirror/EditorView/mode/ecl/ecl.js
@@ -1,17 +1,9 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
 "use strict";
 
-CodeMirror.defineMode("ecl", function(config) {
+function ExportedMode(config) {
 
   function words(str) {
     var obj = {}, words = str.split(" ");
@@ -199,8 +191,4 @@ CodeMirror.defineMode("ecl", function(config) {
 
     electricChars: "{}"
   };
-});
-
-CodeMirror.defineMIME("text/x-ecl", "ecl");
-
-});
+}
