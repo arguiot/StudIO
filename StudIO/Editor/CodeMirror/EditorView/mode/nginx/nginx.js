7e9539dd585ba7f1eb68a584dfc669d5230510ad
Arthur Guiot <arguiot@gmail.com>
2019-04-18 21:56:02 -0700

    Converted Nginx

--- StudIO/Editor/CodeMirror/EditorView/mode/nginx/nginx.js
+++ StudIO/Editor/CodeMirror/EditorView/mode/nginx/nginx.js
@@ -1,17 +1,7 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
-"use strict";
-
-CodeMirror.defineMode("nginx", function(config) {
+function ExportedMode(config) {
 
   function words(str) {
     var obj = {}, words = str.split(" ");
@@ -171,8 +161,4 @@ CodeMirror.defineMode("nginx", function(config) {
 
     electricChars: "}"
   };
-});
-
-CodeMirror.defineMIME("text/x-nginx-conf", "nginx");
-
-});
+}
