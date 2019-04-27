cfd4251fcc1b1283bb0a1b5f02e4768171beaaa4
Arthur Guiot <arguiot@gmail.com>
2019-04-27 12:24:09 +0800

    Animation is fucked, but SmartKeyboard is showing prompt after touching the bulb light

--- StudIO/Editor/Controllers/SmartKeyboard.swift
+++ StudIO/Editor/Controllers/SmartKeyboard.swift
@@ -18,8 +18,32 @@ class SmartKeyboard: UIView {
     }
     */
 
+    @IBOutlet weak var bulb: UIButton!
+    @IBOutlet weak var questionInput: UITextField!
+    var state = false
     @IBAction func cheatCode(_ sender: Any) {
-        print("It Fucking works!!")
+        questionInput.isHidden = state
+        state = !state
+        
+        if (state == true) {
+            questionInput.center.y -= self.bounds.height
+            questionInput.alpha = 0
+            UIView.animate(withDuration: 0.5) {
+                self.questionInput.center.y += self.bounds.height
+                self.questionInput.alpha = 1
+                self.bulb.tintColor = .blue
+            }
+        } else {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.questionInput.center.y -= self.bounds.height
+                self.questionInput.alpha = 0
+                self.bulb.tintColor = .black
+            }) { (done) in
+                self.questionInput.center.y += self.bounds.height
+            }
+            
+        }
+        
     }
     
 }
