8b22ac0f855fffd7c38083383bd80f312c4be74a
Arthur Guiot <arguiot@gmail.com>
2019-05-17 20:38:50 +0800

    Removed CatchJS

--- StudIO/Editor/CodeMirror/EditorView/runtime.js
+++ StudIO/Editor/CodeMirror/EditorView/runtime.js
@@ -1,37 +1,3 @@
 var ed = new StudIO.editor()
 
 var EditorSettings = {}
-
-const d = new Catch(err => {
-	// Do something when there is an error
-	const name = err.errObj.message || err.err
-	const {
-		src,
-		line,
-		column,
-		time,
-		userAgent
-	} = err
-	const json = {
-		err: name,
-		src: src,
-		line: line,
-		column: column,
-		time: time,
-		userAgent: userAgent
-	};
-	(async () => {
-		const rawResponse = await fetch('https://errors.studiocode.app/', {
-			method: 'POST',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				name: name,
-				"JSON": json
-			})
-		});
-		const content = await rawResponse.json();
-	})();
-})
