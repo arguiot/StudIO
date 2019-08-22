8e9403e5132877e75cc4b321299a992e719e7a93
Arthur Guiot <arguiot@gmail.com>
2019-08-22 09:34:32 -0400

    Improved mode meta file efficiency

--- StudIO/Editor/CodeMirror/EditorView/mode/meta.js
+++ StudIO/Editor/CodeMirror/EditorView/mode/meta.js
@@ -1,16 +1,14 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
-(function(mod) {
+(mod => {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
     mod(require("../lib/codemirror"));
   else if (typeof define == "function" && define.amd) // AMD
     define(["../lib/codemirror"], mod);
   else // Plain browser env
     mod(CodeMirror);
-})(function(CodeMirror) {
-  "use strict";
-
+})(CodeMirror => {
   CodeMirror.modeInfo = [
     {name: "APL", mime: "text/apl", mode: "apl", ext: ["dyalog", "apl"]},
     {name: "PGP", mimes: ["application/pgp", "application/pgp-encrypted", "application/pgp-keys", "application/pgp-signature"], mode: "asciiarmor", ext: ["asc", "pgp", "sig"]},
@@ -169,48 +167,48 @@
     {name: "msgenny", mime: "text/x-msgenny", mode: "mscgen", ext: ["msgenny"]}
   ];
   // Ensure all modes have a mime property for backwards compatibility
-  for (var i = 0; i < CodeMirror.modeInfo.length; i++) {
-    var info = CodeMirror.modeInfo[i];
+  for (let i = 0; i < CodeMirror.modeInfo.length; i++) {
+    const info = CodeMirror.modeInfo[i];
     if (info.mimes) info.mime = info.mimes[0];
   }
 
-  CodeMirror.findModeByMIME = function(mime) {
+  CodeMirror.findModeByMIME = mime => {
     mime = mime.toLowerCase();
-    for (var i = 0; i < CodeMirror.modeInfo.length; i++) {
-      var info = CodeMirror.modeInfo[i];
+    for (let i = 0; i < CodeMirror.modeInfo.length; i++) {
+      const info = CodeMirror.modeInfo[i];
       if (info.mime == mime) return info;
-      if (info.mimes) for (var j = 0; j < info.mimes.length; j++)
+      if (info.mimes) for (let j = 0; j < info.mimes.length; j++)
         if (info.mimes[j] == mime) return info;
     }
     if (/\+xml$/.test(mime)) return CodeMirror.findModeByMIME("application/xml")
     if (/\+json$/.test(mime)) return CodeMirror.findModeByMIME("application/json")
   };
 
-  CodeMirror.findModeByExtension = function(ext) {
+  CodeMirror.findModeByExtension = ext => {
 	ext = ext.toLowerCase()
-    for (var i = 0; i < CodeMirror.modeInfo.length; i++) {
-      var info = CodeMirror.modeInfo[i];
-      if (info.ext) for (var j = 0; j < info.ext.length; j++)
+    for (let i = 0; i < CodeMirror.modeInfo.length; i++) {
+      const info = CodeMirror.modeInfo[i];
+      if (info.ext) for (let j = 0; j < info.ext.length; j++)
         if (info.ext[j] == ext) return info;
     }
   };
 
-  CodeMirror.findModeByFileName = function(filename) {
-    for (var i = 0; i < CodeMirror.modeInfo.length; i++) {
-      var info = CodeMirror.modeInfo[i];
+  CodeMirror.findModeByFileName = filename => {
+    for (let i = 0; i < CodeMirror.modeInfo.length; i++) {
+      const info = CodeMirror.modeInfo[i];
       if (info.file && info.file.test(filename)) return info;
     }
-    var dot = filename.lastIndexOf(".");
-    var ext = dot > -1 && filename.substring(dot + 1, filename.length);
+    const dot = filename.lastIndexOf(".");
+    const ext = dot > -1 && filename.substring(dot + 1, filename.length);
     if (ext) return CodeMirror.findModeByExtension(ext);
   };
 
-  CodeMirror.findModeByName = function(name) {
+  CodeMirror.findModeByName = name => {
     name = name.toLowerCase();
-    for (var i = 0; i < CodeMirror.modeInfo.length; i++) {
-      var info = CodeMirror.modeInfo[i];
+    for (let i = 0; i < CodeMirror.modeInfo.length; i++) {
+      const info = CodeMirror.modeInfo[i];
       if (info.name.toLowerCase() == name) return info;
-      if (info.alias) for (var j = 0; j < info.alias.length; j++)
+      if (info.alias) for (let j = 0; j < info.alias.length; j++)
         if (info.alias[j].toLowerCase() == name) return info;
     }
   };
