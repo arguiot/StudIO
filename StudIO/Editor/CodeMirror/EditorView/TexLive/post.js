1065575ff56072cf255c731ebf09b099a774ca80
Arthur Guiot <arguiot@gmail.com>
2020-02-04 18:02:45 -0500

    Cleaned dependencies

--- StudIO/Editor/CodeMirror/EditorView/TexLive/post.js
+++
@@ -1,5 +0,0 @@
-self['postMessage'](JSON.stringify({'command': 'ready'}));
-//shouldRunNow = true;
-Module['calledRun']=false;
-Module['thisProgram']='/latex';
-FS.createDataFile("/",Module['thisProgram'],"dummy for kpathsea",true,true);
